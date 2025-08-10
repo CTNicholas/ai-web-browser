9d6b51204a1f4c8b43c469645c255cf1a0633427
Chris Nicholas <ctnicholasdev@gmail.com>
2025-08-10 23:23:15 +0100

    feat: Search icon

--- src/components/AiChatPanel.tsx
+++ src/components/AiChatPanel.tsx
@@ -267,13 +267,11 @@ function MarkdownIcon(props: React.SVGProps<SVGSVGElement>) {
       strokeWidth={2}
       strokeLinecap="round"
       strokeLinejoin="round"
+      className="lucide lucide-search-icon lucide-search"
       {...props}
     >
-      <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-      <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-      <path d="M9 9l1 0" />
-      <path d="M9 13l6 0" />
-      <path d="M9 17l6 0" />
+      <path d="M21 21l-4.34-4.34" />
+      <circle cx={11} cy={11} r={8} />
     </svg>
   );
 }
